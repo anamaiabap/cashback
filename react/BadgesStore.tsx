078c3b85d1e366fe8e8aba6c3a36435debb0e2e9
Ana Maia <ana.baptistao@vtex.com.br>
2021-08-09 17:12:26 -0300

    change RichTextProps and ImageProps to Props

--- react/BadgesStore.tsx
+++ react/BadgesStore.tsx
@@ -4,70 +4,8 @@ import { ConditionLayoutProduct } from 'vtex.condition-layout'
 import type { BadgesProps } from './typings/badgesProps'
 import { conditionsPropsFunction } from './utils/conditionsProps'
 
-const BadgesStore: StorefrontFunctionComponent<BadgesProps> = (
-  props: BadgesProps
-) => {
-  const {
-    font,
-    textAlignment,
-    textColor,
-    textPosition,
-    classes,
-    htmlId,
-    alt = '',
-    maxWidth,
-    maxHeight,
-    minWidth,
-    minHeight,
-    width,
-    height,
-    srcSet = '',
-    sizes = '',
-    link,
-    title,
-    experimentalPreventLayoutShift,
-    analyticsProperties = 'none',
-    promotionId,
-    promotionName,
-    promotionPosition,
-    classesImage,
-    preload,
-  } = props
-
-  const richTextProps = {
-    font,
-    textAlignment,
-    textColor,
-    textPosition,
-    classes,
-    htmlId,
-  }
-
-  const imageProps = {
-    alt,
-    maxWidth,
-    maxHeight,
-    minWidth,
-    minHeight,
-    width,
-    height,
-    title,
-    classesImage,
-    preload,
-    srcSet,
-    sizes,
-    link,
-    experimentalPreventLayoutShift,
-    analyticsProperties,
-    promotionId,
-    promotionName,
-    promotionPosition,
-  }
-
-  const conditionsPropsValues = conditionsPropsFunction(
-    richTextProps,
-    imageProps
-  )
+const BadgesStore: StorefrontFunctionComponent<BadgesProps> = (props: any) => {
+  const conditionsPropsValues = conditionsPropsFunction(props)
 
   return (
     <div className="container">
--- react/utils/conditionsProps.tsx
+++ react/utils/conditionsProps.tsx
@@ -4,12 +4,8 @@ import { index as RichText } from 'vtex.rich-text'
 import { Image } from 'vtex.store-image'
 
 import searchMasterdata from '../queries/searchMasterdata.gql'
-import type { ImagePropsType } from '../typings/imageProps'
 
-export const conditionsPropsFunction = (
-  richTextProps: RichTextProps,
-  imageProps: ImagePropsType
-) => {
+export const conditionsPropsFunction = (props: any) => {
   const { data } = useQuery(searchMasterdata)
 
   const conditionsProps = useMemo(() => {
@@ -19,54 +15,26 @@ export const conditionsPropsFunction = (
   }, [data])
 
   const conditionsMap = conditionsProps.map((element: any) => {
-    return conditionsPropsValues(element, richTextProps, imageProps)
+    return conditionsPropsValues(element, props)
   })
 
   return conditionsMap
 }
 
-function conditionsPropsValues(
-  data: any,
-  richTextProps: RichTextProps,
-  imageProps: ImagePropsType
-) {
+function conditionsPropsValues(data: any, props: any) {
   const values = {
     conditions: conditionsFunction(data?.simpleStatements),
     matchType: data?.operator,
     Then: () => {
       if (data?.type === 'text') {
-        return (
-          <RichText
-            text={data?.content}
-            font={richTextProps.font}
-            textColor={richTextProps.textColor}
-            htmlId={richTextProps.htmlId}
-            textAlignment={richTextProps.textAlignment}
-            textPosition={richTextProps.textPosition}
-            classes={richTextProps.classes}
-          />
-        )
+        return <RichText text={data?.content} {...props.text} />
       }
 
       if (data?.type === 'html') {
         return <div dangerouslySetInnerHTML={createMarkup(data?.content)} />
       }
 
-      return (
-        <Image
-          src={data?.content}
-          height={imageProps.height}
-          width={imageProps.width}
-          maxWidth={imageProps.maxWidth}
-          maxHeight={imageProps.maxHeight}
-          minWidth={imageProps.minWidth}
-          minHeight={imageProps.minHeight}
-          alt={imageProps.alt}
-          title={imageProps.title}
-          classes={imageProps.classesImage}
-          preload={imageProps.preload}
-        />
-      )
+      return <Image src={data?.content} {...props.image} />
     },
   }
 
