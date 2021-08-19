81d2bfa0035985331253268bc7952afe202d8b0d
Ana Maia <ana.baptistao@vtex.com.br>
2021-08-19 16:17:09 -0300

    withModifiers

--- react/BadgesStore.tsx
+++ react/BadgesStore.tsx
@@ -4,20 +4,19 @@ import { useCssHandles } from 'vtex.css-handles'
 
 import { conditionsPropsFunction } from './utils/conditionsProps'
 
-const CSS_HANDLES = [
-  'allBadgesContainer',
-  'badgeContainer',
-  'badgesText',
-  'badgesHtml',
-  'badgesImage',
-] as const
+const CSS_HANDLES = ['allBadgesContainer', 'badgeContainer', 'badges'] as const
 
 const BadgesStore: StorefrontFunctionComponent = (props: any) => {
-  const handles = useCssHandles(CSS_HANDLES)
-  const conditionsPropsValues = conditionsPropsFunction(props, handles)
+  const cssHandles: any = useCssHandles(CSS_HANDLES)
+
+  const conditionsPropsValues = conditionsPropsFunction(
+    props,
+    cssHandles.handles,
+    cssHandles.withModifiers
+  )
 
   return (
-    <div className={handles.allBadgesContainer}>
+    <div className={cssHandles.handles.allBadgesContainer}>
       {conditionsPropsValues.map((element: any) => {
         return <ConditionLayoutProduct {...element} />
       })}
