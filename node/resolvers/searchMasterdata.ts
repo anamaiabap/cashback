5bfe4c51294869de1ddd99fa1b6bb06f9f82c605
Ana Maia <ana.baptistao@vtex.com.br>
2021-08-23 17:19:55 -0300

    searchMasterdata with filter where

--- node/resolvers/searchMasterdata.ts
+++ node/resolvers/searchMasterdata.ts
@@ -1,10 +1,12 @@
-export async function searchMasterdata(_: unknown, __: unknown, ctx: Context) {
-  return ctx.clients.badges.search({ page: 1, pageSize: 1000 }, [
-    'id',
-    'name',
-    'content',
-    'operator',
-    'simpleStatements',
-    'type',
-  ])
+export async function searchMasterdata(
+  _: unknown,
+  where: { where: string },
+  ctx: Context
+) {
+  return ctx.clients.badges.search(
+    { page: 1, pageSize: 1000 },
+    ['id', 'name', 'content', 'operator', 'simpleStatements', 'type'],
+    undefined,
+    where.where
+  )
 }
